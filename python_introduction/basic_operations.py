2541400676d2cbf2b011932adb37025f113ec3fd
Rotimi <rotimi5574@gmail.com>
2025-11-07 22:53:33 +0100

    git commit - m "basic_operations.py"

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+number1 = 10
+number2 = 5
+sum_result = number1 + number2
+difference_result = number1 - number2
+product_result = number1 * number2
+print(sum_result)
+15
+print(difference_result)
+5
+print(product_result)
+50
