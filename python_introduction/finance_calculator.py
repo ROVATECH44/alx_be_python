afe2247663497ebc04a1b1f0cf34e9f5aca96366
Rotimi <rotimi5574@gmail.com>
2025-11-08 00:09:44 +0100

    git commit -m "finance_calculator.py"

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,10 @@
+monthly_income = float(input("Enter your monthly income: "))
+5000
+monthly_expenses = float(input("Enter your total monthly expenses: "))
+4000
+monthly_savings = monthly_income - monthly_expenses
+print(monthly_savings)
+1000.0
+projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
+print(projected_savings)
+12600.0
