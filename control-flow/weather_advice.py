6185a3dff188aa7b5298ca4458faf7541b4871b5
Rotimi <rotimi5574@gmail.com>
2025-11-16 21:28:41 +0100

    git commit -m "weather_advice.py"

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,8 @@
+weather = str(input("What is the weather like today? "))
+Sunny
+if weather >= 50:
+print("Wear a t-shirt and sunglasses. ")
+else:
+print("Sorry, i don't have recommendations for this weather. ")
+
+
