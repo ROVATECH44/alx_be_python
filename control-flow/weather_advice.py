0d2c3f376313ccbb8f47dc2eb039d193f6174d45
Rotimi Emmanuel Osaosna <96309377+ROVATECH44@users.noreply.github.com>
2025-11-17 09:02:59 +0100

    Delete control-flow directory

--- control-flow/weather_advice.py
+++
@@ -1,8 +0,0 @@
-weather = str(input("What is the weather like today? "))
-Sunny
-if weather >= 50:
-print("Wear a t-shirt and sunglasses. ")
-else:
-print("Sorry, i don't have recommendations for this weather. ")
-
-
