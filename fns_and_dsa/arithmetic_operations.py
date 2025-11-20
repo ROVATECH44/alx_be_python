51818bb9ee4ea19093e5efcbff59abf911595a51
Rotimi <rotimi5574@gmail.com>
2025-11-20 16:38:52 +0100

    git commit -m "arithmetic_operations.py"

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -11,3 +11,15 @@ return "Division by zero error"
 return num1 / num2
 else:
 return "Invalid operation"
+
+# from arithmetic_operation import perform_operation
+def  main():
+print("Arithmetic Operation")
+num1 = float(input(10))
+num2 = float(input(5))
+operation = input('add'('add', 'subtract', 'multiply', 'divide'): ).strip().lower()
+result = perform_operation(10, 5, 'add')
+print(f"Result: {result}")
+if --name-- == "--main--":
+    main()
+    
