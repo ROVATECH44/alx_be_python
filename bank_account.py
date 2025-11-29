eedbc647426cdc5a9c8359f2deddfffab51efeda
Rotimi <rotimi5574@gmail.com>
2025-11-29 21:32:52 +0100

    git commit -m "bank_account.py"

---
+++ bank_account.py
@@ -0,0 +1,4 @@
+class BankAccount:
+    def __init__(self, initial_balance=0):
+        # Initialize the account balance, defaulting to zero if not provided
+        self.account_balance = initial_balance
